2ad3ddc24037db0d78a2cd224950d69c00579c3d
Mikk Romulus <mikk@clarifiedsecurity.com>
2023-06-08 09:53:42 +0300

    fix: clearing behaviour on tom-select
    Don't allow clearing the field if no <option> exists with empty value.
    This fixes the error when attempting to clear check source or destination

--- app/javascript/controllers/select_controller.js
+++ app/javascript/controllers/select_controller.js
@@ -4,10 +4,15 @@ import TomSelect from "tom-select";
 export default class extends Controller {
   connect() {
     if (!this.element.hidden) {
+      const hasEmptyOption =
+        this.element.querySelectorAll("option[value='']").length > 0;
       new TomSelect(this.element, {
-        allowEmptyOption: true,
+        allowEmptyOption: hasEmptyOption,
         maxOptions: null,
         wrapperClass: "ts-wrapper form-input",
+        onDelete: function (values, event) {
+          return !(values.length == 1 && !hasEmptyOption);
+        },
       });
     }
   }
