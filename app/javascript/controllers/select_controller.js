f57ffd3f812f3a0c0a4ba7dab53367606cf63ce5
Mikk Romulus <mikk@clarifiedsecurity.com>
2023-06-09 10:36:27 +0300

    fix: tom-select for multi-select fields
    Could not remove items before

--- app/javascript/controllers/select_controller.js
+++ app/javascript/controllers/select_controller.js
@@ -6,12 +6,13 @@ export default class extends Controller {
     if (!this.element.hidden) {
       const hasEmptyOption =
         this.element.querySelectorAll("option[value='']").length > 0;
+      const ismultiple = this.element.multiple;
       new TomSelect(this.element, {
         allowEmptyOption: hasEmptyOption,
         maxOptions: null,
         wrapperClass: "ts-wrapper form-input",
         onDelete: function (values, event) {
-          return !(values.length == 1 && !hasEmptyOption);
+          return ismultiple || !(values.length == 1 && !hasEmptyOption);
         },
       });
     }
