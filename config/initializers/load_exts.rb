ac8ff004893c29540bfd091e79622c8fd484c2d8
Mikk Romulus <mikk@clarifiedsecurity.com>
2023-09-11 14:46:31 +0300

    fix: fix paper_trail extensions in dev mode

--- config/initializers/load_exts.rb
+++ config/initializers/load_exts.rb
@@ -4,3 +4,8 @@ require 'ipaddress_extension'
 require 'version_number_loader'
 
 VersionNumberLoader.new.start
+
+# needs to be manually loaded for some reason
+if Rails.env.development?
+  require 'paper_trail/version'
+end
