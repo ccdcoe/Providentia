0bfdb5bacfd67a1fc5cdb3645d33da223c73359e
Mikk Romulus <mikk@clarifiedsecurity.com>
2023-10-23 09:56:37 +0300

    fix: Don't allow removing numbered_by when it is forced by a NIC

--- app/calculation/numbering_options.rb
+++ app/calculation/numbering_options.rb
@@ -2,17 +2,29 @@
 
 class NumberingOptions < Patterns::Calculation
   def result
-    (available_actors + available_number_configs).map do |item|
-      text_value = if item.is_a?(Actor)
-        item.name
-      else
-        "#{item.actor.name} - #{item.name}"
-      end
-      [I18n.t('deploy_modes.per_item', item: text_value), item.to_gid]
-    end
+    options[:hash].merge(
+      collection:,
+      include_blank:,
+      disabled: forced_numbering?
+    )
   end
 
   private
+    def collection
+      (available_actors + available_number_configs).map do |item|
+        text_value = if item.is_a?(Actor)
+          item.name
+        else
+          "#{item.actor.name} - #{item.name}"
+        end
+        [I18n.t('deploy_modes.per_item', item: text_value), item.to_gid]
+      end
+    end
+
+    def include_blank
+      I18n.t('deploy_modes.single') unless forced_numbering?
+    end
+
     def forced_numbering?
       numbered_networks.any?
     end
