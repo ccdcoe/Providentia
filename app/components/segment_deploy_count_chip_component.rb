34275a777c2ab2a68846098d324b17dec567d32f
Mikk Romulus <mikk@clarifiedsecurity.com>
2024-01-23 16:33:25 +0200

    fix(ui): deploy count chip
    * fix missing translation in tooltip
    * fix deploy count

--- app/components/segment_deploy_count_chip_component.rb
+++ app/components/segment_deploy_count_chip_component.rb
@@ -1,43 +1,37 @@
 # frozen_string_literal: true
 
 class SegmentDeployCountChipComponent < ViewComponent::Base
+  attr_reader :title, :value, :css_color_classes
+
   def initialize(vm:, network:)
     @vm = vm
     @network = network
+
+    generate_attributes
   end
 
   def render?
-    !vm_single_instance? && (
-      @vm.custom_instance_count || !@network.numbered? && @vm.deploy_count > 1
-    )
+    numbered_by_sequential? || network_not_numbered_and_actor_is_numbered?
   end
 
   private
-    def vm_single_instance?
-      !@vm.numbered_actor && !@vm.custom_instance_count
+    def numbered_by_sequential?
+      @vm.custom_instance_count.to_i > 1
     end
 
-    def title
-      if @vm.custom_instance_count
-        'Custom instance count'
-      elsif !@network.numbered? && @vm.deploy_count > 1
-        I18n.t('deploy_modes.actor', actor: @vm.numbered_actor.name)
-      end
+    def network_not_numbered_and_actor_is_numbered?
+      !@network.numbered? && @vm.deploy_count > 1
     end
 
-    def value
-      if @vm.custom_instance_count
-        @vm.custom_instance_count
-      elsif !@network.numbered? && @vm.deploy_count > 1
-        @vm.deploy_count
-      end
-    end
-
-    def css_color_classes
-      if @vm.custom_instance_count
-        'bg-green-100 text-green-500'
-      else
-        'bg-indigo-100 text-indigo-500'
+    def generate_attributes
+      if numbered_by_sequential?
+        @title = 'Custom instance count'
+        @value = @vm.custom_instance_count - 1
+        @css_color_classes = 'bg-green-100 text-green-500'
+      elsif network_not_numbered_and_actor_is_numbered?
+        @title = I18n.t('deploy_modes.per_item', item: @vm.numbered_actor.name)
+        @value = @vm.deploy_count - 1
+        @css_color_classes = 'bg-indigo-100 text-indigo-500'
       end
     end
 end
